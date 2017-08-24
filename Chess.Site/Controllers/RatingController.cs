17266f6562bf1a17ca5ad8f710e1e574cdb23922
rusakov <rusakov@byndyusoft.com>
2017-08-24 18:13:23 +0500

    Добавил вывод формы добавления результата партии

---
+++ Chess.Site/Controllers/RatingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Chess.Site.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Chess.Site.Controllers
+{
+    public class RatingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new RatingViewModel
+            {
+                Players = new []
+                {
+                    new SelectListItem{Text = "Первый", Value = "first"},
+                    new SelectListItem{Text = "Второй", Value = "second"},
+                }
+            });
+        }
+    }
+}
---
+++ Chess.Site/Models/RatingViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Chess.Site.Models
+{
+    public class RatingViewModel
+    {
+        public SelectListItem[] Players { get; set; }
+        public string FirstPlayer { get; set; }
+        public string SecondPlayer { get; set; }
+
+        public GameResult Result { get; set; }
+    }
+
+    public enum GameResult
+    {
+        [Display(Name = "Первый победил")]
+        FirstWin,
+
+        [Display(Name = "Ничья")]
+        NoWins,
+
+        [Display(Name = "Второй победил")]
+        LastWin
+    }
+}
